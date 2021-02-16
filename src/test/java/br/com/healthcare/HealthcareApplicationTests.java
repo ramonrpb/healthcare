0e5b468df8aa72bbe8ba83e90e871c9de47752ca
Ramon Barros <ramonrpb@hotmail.com>
2021-02-15 21:09:05 -0300

    Projeto criado

---
+++ src/test/java/br/com/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.healthcare;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
